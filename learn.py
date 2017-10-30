448efa3a11708204150e4e02499bac9732d43e4a
Mitsuru Kusumoto <kusumoto.mitsuru@gmail.com>
2017-10-30 18:37:11 +0900

    minor refactoring

--- learn.py
+++ learn.py
@@ -20,7 +20,6 @@ class PreprocessedDataset(chainer.dataset.DatasetMixin):
             f = open(filename, 'rb')
             s = bitarray(f.read())
             self.data.extend(s)
-        print(len(self.data))
 
     def __len__(self):
         return len(self.data) // (2 * self.per)
@@ -57,8 +56,12 @@ class RankLoss(chainer.Chain):
         y_high = self.predictor(x_high)
         y_low = self.predictor(x_low)
         diff = y_high - y_low
+
         self.loss = F.average(F.log(1 + F.exp(-diff)))
         reporter.report({'loss': self.loss}, self)
+
+        self.accuracy = sum(diff.data > 0)[0] / diff.shape[0]
+        reporter.report({'accuracy': self.accuracy}, self)
 
         return self.loss
 
@@ -67,43 +70,38 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--batchsize', type=int, default=128)
     parser.add_argument('--epoch', type=int, default=20)
-    parser.add_argument('--gpu', type=int, default=0)
+    parser.add_argument('--gpu', type=int, default=-1)
     parser.add_argument('--loaderjob', '-j', type=int, default=1)
     parser.add_argument('--dim', type=int, default=8184)
     parser.add_argument('--train', type=str, nargs='+', default=None)
-    parser.add_argument('--test', type=str, nargs='+', default=None)
+    parser.add_argument('--val', type=str, nargs='+', default=None)
     args = parser.parse_args()
 
     model = RankLoss(LinearRegression(args.dim))
     if args.gpu >= 0:
         chainer.cuda.get_device_from_id(args.gpu).use()
         model.to_gpu()
-    print('model: done')
 
     optimizer = chainer.optimizers.Adam()
     optimizer.setup(model)
 
     train = PreprocessedDataset(args.train, args.dim)
-    print('load train: done')
-    test = PreprocessedDataset(args.test, args.dim)
-    print('load test: done')
+    val = PreprocessedDataset(args.val, args.dim)
+    print('dataset load: done')
 
-    """
-    train_iter = chainer.iterators.SerialIterator(train, args.batchsize)
-    test_iter = chainer.iterators.SerialIterator(test, args.batchsize,
-                                                 repeat=False, shuffle=False)
-    """
     train_iter = chainer.iterators.MultiprocessIterator(
         train, args.batchsize, n_processes=args.loaderjob)
-    test_iter = chainer.iterators.MultiprocessIterator(
-        test, args.batchsize, repeat=False, n_processes=args.loaderjob)
+    val_iter = chainer.iterators.MultiprocessIterator(
+        val, args.batchsize, repeat=False, n_processes=args.loaderjob)
 
     updater = training.StandardUpdater(train_iter, optimizer, device=args.gpu)
     trainer = training.Trainer(updater, (args.epoch, 'epoch'))
-    trainer.extend(extensions.Evaluator(test_iter, model, device=args.gpu))
+    trainer.extend(extensions.Evaluator(val_iter, model, device=args.gpu))
     trainer.extend(extensions.LogReport())
     trainer.extend(extensions.PrintReport(
-        ['epoch', 'main/loss', 'validation/main/loss', 'elapsed_time']))
+        ['epoch', 'main/loss', 'validation/main/loss',
+         'main/accuracy', 'validation/main/accuracy',
+         'elapsed_time']))
     trainer.extend(extensions.ProgressBar())
 
     trainer.run()
