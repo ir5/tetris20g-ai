bd552d6772703cb082f56cef24afe516f14d4161
Mitsuru Kusumoto <kusumoto.mitsuru@gmail.com>
2017-10-31 08:57:10 +0900

    Closed loop test

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ mod regressor;
 mod dataset_generator;
 
 use core::Field;
+use core::fix_piece;
 use chrono::prelude::*;
 use human_manipulation::Game;
 use display::Display;
@@ -67,6 +68,12 @@ enum Opt {
         #[structopt(long = "file")]
         file: String,
     },
+
+    #[structopt(name = "closed-loop")]
+    ClosedLoop {
+        #[structopt(long = "file")]
+        file: String,
+    },
 }
 
 fn main() {
@@ -90,6 +97,7 @@ fn main() {
             generate_dataset(&input, &output, drop_rate, weights_file);
         }
         Opt::CheckWeights { file } => check_weights(file),
+        Opt::ClosedLoop { file } => closed_loop(file),
     }
 }
 
@@ -183,6 +191,45 @@ fn check_weights(file: String) {
         println!("Value = {}", value);
 
         let _ = display.wait_key();
+    }
+}
+
+fn closed_loop(file: String) {
+    let mut regressor = LinearRegressor::new();
+    regressor.load(&file);
+
+    let mut field = core::EMPTY_FIELD;
+    let mut rng = rand::thread_rng();
+    let m: Vec<u8> = "IOSZJLT".bytes().collect();
+    let mut seq = vec![];
+    for _ in 0..10000 {
+        seq.push(*rng.choose(&m).unwrap());
+    }
+    let display = Display::new();
+
+    for i in 0..(seq.len() - 1) {
+        let next_piece = seq[i];
+        let next2_piece = seq[i + 1];
+        let candidates = enumerate_multi(&field, &vec![next_piece, next2_piece]);
+        // find maximum value candidate
+        let mut best_value: f32 = -1e10;
+        let mut best = vec![];
+        for candidate in candidates {
+            let value = regressor.predict(&candidate[1].new_field);
+            if value > best_value {
+                best_value = value;
+                best = candidate.clone();
+            }
+        }
+        let state = &best[0].last_state;
+
+        println!("Step {} : Value = {}", i, best_value);
+
+        display.draw(&field, &state, Some(next2_piece));
+        let _ = display.wait_key();
+
+        let (new_field, _) = fix_piece(&field, &state);
+        field = new_field.clone();
     }
 }
 
